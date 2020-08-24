47ce5261454dd29a5ac322381084ab71c77a5203
daniel makarzec <danielmakarzec@gmail.com>
2020-08-25 00:56:00 +0100

    Rails: Prepare the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,433 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+candies = [
+  {
+    id: 1,
+    name: "Skittles",
+    file: "1.skittles.jpg"
+  },
+  {
+    id: 2,
+    name: "Jolly Rancher",
+    file: "2-jolly-rancher.jpg"
+  },
+  {
+    id: 3,
+    name: "Milk Duds​",
+    file: "3-milk-duds​.jpg"
+  },
+  {
+    id: 4,
+    name: "Baby Ruth",
+    file: "4-baby-ruth.jpg"
+  },
+  {
+    id: 5,
+    name: "Starburst",
+    file: "5-starburst.jpg"
+  },
+  {
+    id: 6,
+    name: "SweetTarts",
+    file: "6-sweettarts.jpg"
+  },
+  {
+    id: 7,
+    name: "Bit o Honey​",
+    file: "7-bit-o-honey​.jpg"
+  },
+  {
+    id: 8,
+    name: "Werther's Original",
+    file: "8-werther's-original.png"
+  },
+  {
+    id: 9,
+    name: "Riesen​",
+    file: "9-riesen​.jpg"
+  },
+  {
+    id: 10,
+    name: "Pop Rocks​",
+    file: "10-pop-rocks​.jpg"
+  },
+  {
+    id: 11,
+    name: "Nestle Crunch Bar",
+    file: "11-nestle-crunch-bar.jpg"
+  },
+  {
+    id: 12,
+    name: "Jawbreakers",
+    file: "12-jawbreakers.jpg"
+  },
+  {
+    id: 13,
+    name: "Twizzlers",
+    file: "13-twizzlers.jpg"
+  },
+  {
+    id: 14,
+    name: "Toblerone (Dark)",
+    file: "14-toblerone-(dark).png"
+  },
+  {
+    id: 15,
+    name: "Smarties",
+    file: "15-smarties.jpg"
+  },
+  {
+    id: 16,
+    name: "Mr",
+    file: "16-mr.jpg"
+  },
+  {
+    id: 17,
+    name: "Pez Candy Dispenser",
+    file: "17-pez-candy-dispenser.jpg"
+  },
+  {
+    id: 18,
+    name: "Blow Pops",
+    file: "18-blow-pops.jpg"
+  },
+  {
+    id: 19,
+    name: "Almond Joy",
+    file: "19-almond-joy.jpg"
+  },
+  {
+    id: 20,
+    name: "Sour Patch Kids​",
+    file: "20-sour-patch-kids​.jpg"
+  },
+  {
+    id: 21,
+    name: "Atomic Fireball",
+    file: "21-atomic-fireball.jpg"
+  },
+  {
+    id: 22,
+    name: "Payday",
+    file: "22-payday.png"
+  },
+  {
+    id: 23,
+    name: "Reese's Pieces",
+    file: "23-reese's-pieces.jpg"
+  },
+  {
+    id: 24,
+    name: "Junior Mints",
+    file: "24-junior-mints.jpg"
+  },
+  {
+    id: 25,
+    name: "Milky Way",
+    file: "25-milky-way.jpg"
+  },
+  {
+    id: 26,
+    name: "Cadbury Egg",
+    file: "26-cadbury-egg.jpg"
+  },
+  {
+    id: 27,
+    name: "Tootsie Pop​",
+    file: "27-tootsie-pop​.jpg"
+  },
+  {
+    id: 28,
+    name: "York Peppermint Patties",
+    file: "28-york-peppermint-patties.jpg"
+  },
+  {
+    id: 29,
+    name: "Three Musketeers",
+    file: "29-three-musketeers.jpg"
+  },
+  {
+    id: 30,
+    name: "Twix",
+    file: "30-twix.jpg"
+  },
+  {
+    id: 31,
+    name: "Kit Kat ",
+    file: "31-kit-kat .jpg"
+  },
+  {
+    id: 32,
+    name: "M&Ms",
+    file: "32-m&ms.jpg"
+  },
+  {
+    id: 33,
+    name: "Snickers​",
+    file: "33-snickers​.jpg"
+  },
+  {
+    id: 34,
+    name: "Air Heads",
+    file: "34-air-heads.png"
+  },
+  {
+    id: 35,
+    name: "Andes Mints",
+    file: "35-andes-mints.png"
+  },
+  {
+    id: 36,
+    name: "Boston Baked Beans",
+    file: "36-boston-baked-beans.gif"
+  },
+  {
+    id: 37,
+    name: "Hubba Bubba​",
+    file: "37-hubba-bubba​.png"
+  },
+  {
+    id: 38,
+    name: "Charleston Chew​",
+    file: "38-charleston-chew​.png"
+  },
+  {
+    id: 39,
+    name: "Dots​",
+    file: "39-dots​.png"
+  },
+  {
+    id: 40,
+    name: "Chick-o-Stick​",
+    file: "40-chick-o-stick​.jpg"
+  },
+  {
+    id: 41,
+    name: "Gobstopper​",
+    file: "41-gobstopper​.jpg"
+  },
+  {
+    id: 42,
+    name: "Whatchamacallit",
+    file: "42-whatchamacallit.jpg"
+  },
+  {
+    id: 43,
+    name: "Fruit Gushers​",
+    file: "43-fruit-gushers​.jpg"
+  },
+  {
+    id: 44,
+    name: "Fun Dip​",
+    file: "44-fun-dip​.jpg"
+  },
+  {
+    id: 45,
+    name: "Hot Tamales​",
+    file: "45-hot-tamales​.png"
+  },
+  {
+    id: 46,
+    name: "Goo Goo Cluster",
+    file: "46-goo-goo-cluster.jpg"
+  },
+  {
+    id: 48,
+    name: "Mounds",
+    file: "48-mounds.jpg"
+  },
+  {
+    id: 49,
+    name: "Lemonhead",
+    file: "49-lemonhead.png"
+  },
+  {
+    id: 50,
+    name: "Nerds​",
+    file: "50-nerds​.jpg"
+  },
+  {
+    id: 51,
+    name: "Cherry Sours​",
+    file: "51-cherry-sours​.jpg"
+  },
+  {
+    id: 52,
+    name: "Laffy Taffy",
+    file: "52-laffy-taffy.jpg"
+  },
+  {
+    id: 53,
+    name: "100 Grand Bar",
+    file: "53-100-grand-bar.jpg"
+  },
+  {
+    id: 54,
+    name: "Orange Slices",
+    file: "54-orange-slices.jpg"
+  },
+  {
+    id: 55,
+    name: "Razzles​",
+    file: "55-razzles​.png"
+  },
+  {
+    id: 56,
+    name: "Slo Poke​",
+    file: "56-slo-poke​.jpg"
+  },
+  {
+    id: 57,
+    name: "Ring Pop​",
+    file: "57-ring-pop​.jpg"
+  },
+  {
+    id: 58,
+    name: "Skybar",
+    file: "58-skybar.jpg"
+  },
+  {
+    id: 59,
+    name: "Rolo​",
+    file: "59-rolo​.jpg"
+  },
+  {
+    id: 60,
+    name: "Red Hots",
+    file: "60-red-hots.jpg"
+  },
+  {
+    id: 61,
+    name: "Warheads​",
+    file: "61-warheads​.jpg"
+  },
+  {
+    id: 62,
+    name: "Sno-Caps",
+    file: "62-sno-caps.jpg"
+  },
+  {
+    id: 63,
+    name: "Spree",
+    file: "63-spree-​.jpg"
+  },
+  {
+    id: 64,
+    name: "Oh Henry",
+    file: "64-oh-henry.jpg"
+  },
+  {
+    id: 66,
+    name: "Swedish Fish",
+    file: "66-swedish-fish-​.jpg"
+  },
+  {
+    id: 67,
+    name: "Valomilk",
+    file: "67-valomilk.jpg"
+  },
+  {
+    id: 68,
+    name: "Butterfinger",
+    file: "68-butterfinger.JPG"
+  },
+  {
+    id: 69,
+    name: "Lifesavers",
+    file: "69-lifesavers-​.jpg"
+  },
+  {
+    id: 70,
+    name: "5th Avenue Candy Bar",
+    file: "70-5th-avenue-candy-bar.jpg"
+  },
+  {
+    id: 71,
+    name: "Clark Bar",
+    file: "71-clark-bar.jpg"
+  },
+  {
+    id: 72,
+    name: "Rocky Road Candy Bar",
+    file: "72-rocky-road-candy-bar.jpg"
+  },
+  {
+    id: 73,
+    name: "Mike and Ike",
+    file: "73-mike-and-ike.JPG"
+  },
+  {
+    id: 74,
+    name: "Heath",
+    file: "74-heath.jpg"
+  },
+  {
+    id: 75,
+    name: "Skor",
+    file: "75-skor.jpg"
+  },
+  {
+    id: 76,
+    name: "Sugar Babies",
+    file: "76-sugar-babies.png"
+  },
+  {
+    id: 77,
+    name: "Wunderbar",
+    file: "77-wunderbar.JPG"
+  },
+  {
+    id: 78,
+    name: "Mamba",
+    file: "78-mamba.jpg"
+  },
+  {
+    id: 79,
+    name: "Raisinets",
+    file: "79-raisinets.jpg"
+  },
+  {
+    id: 80,
+    name: "Goobers",
+    file: "80-goobers.jpg"
+  },
+  {
+    id: 81,
+    name: "Bun",
+    file: "81-bun.jpg"
+  },
+  {
+    id: 82,
+    name: "Zero Candy Bar",
+    file: "82-zero-candy-bar.jpg"
+  },
+  {
+    id: 83,
+    name: "Mars Bar",
+    file: "83-mars-bar.jpg"
+  },
+  {
+    id: 84,
+    name: "Now and Later",
+    file: "84-now-and-later.jpg"
+  },
+  {
+    id: 85,
+    name: "Take 5",
+    file: "85-take-5.jpg"
+  },
+  {
+    id: 86,
+    name: "Caramello",
+    file: "86-caramello.jpg"
+  },
+  {
+    id: 87,
+    name: "Dove Chocolate Candy Bar",
+    file: "87-dove-chocolate-candy-bar.jpg"
+  }
+]
+
+Candy.destroy_all
+
+candies.each do |candy|
+    Candy.create!(id: candy[:id], name: candy[:name], image_url: candy[:file])
+end
