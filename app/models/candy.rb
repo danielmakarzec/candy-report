dd9bcd3ad5ed768132783e115807a03f730eb141
daniel makarzec <danielmakarzec@gmail.com>
2020-08-25 00:19:58 +0100

    Rails: Create model-method arv_score

--- app/models/candy.rb
+++ app/models/candy.rb
@@ -3,12 +3,16 @@ class Candy < ApplicationRecord
 
     validates_presence_of :name
     validates_presence_of :image_url
+    validates :name, uniqueness: { case_sensitive: false }
 
     before_create :slugify
 
     def slugify
-        self.slug = name.parameterize
+        self.slug = name.parameterize if self.name
     end
 
+    def avg_score
+        reviews.average(:score).round(2).to_f
+    end
 
 end
--- spec/models/candy_spec.rb
+++ spec/models/candy_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Candy, type: :model do
-  let(:candy) { Candy.create!(name: "Candy Name", image_url: '/image_path')}
+  let(:candy) { Candy.create!(id: 1, name: "Candy Name", image_url: '/image_path')}
 
+  # validation
   it "is valid with valid attributes" do
     expect(candy).to be_valid
   end
@@ -10,6 +11,8 @@ RSpec.describe Candy, type: :model do
     candy.name = nil
     expect(candy).to_not be_valid
   end
+  it { should validate_uniqueness_of(:name).case_insensitive }
+
   it "is not valid without a image_url" do
     candy.image_url = nil
     expect(candy).to_not be_valid
@@ -24,6 +27,15 @@ RSpec.describe Candy, type: :model do
   describe "#slugify" do
     it "should return serialized slug based on the name on a candy" do
       expect(candy.slug).to eq('candy-name')
+    end
+  end
+
+  describe "#avg_score" do
+    it 'returns average value of all scores' do
+      candy
+      Review.create!(candy_id: 1, title: 'title', description: 'description', score: 1)
+      Review.create!(candy_id: 1, title: 'title', description: 'description', score: 5)
+      expect(candy.avg_score).to eq(3.0)
     end
   end
 end
